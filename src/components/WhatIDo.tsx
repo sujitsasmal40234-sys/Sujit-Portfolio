d0a3627c842c31ec62abf1f0619e6c1e8312a13e
Sujit Sasmal <sujitsasmal40234@gmail.com>
2026-03-19 16:35:47 +0530

    Restore all responsiveness fixes per user request

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -28,7 +28,7 @@ const WhatIDo = () => {
     <div className="whatIDO">
       <div className="what-box">
         <h2 className="title">
-          W<span className="hat-h2">HAT</span>
+          W<span className="hat-h2">HAT </span>
           <div>
             I<span className="do-h2"> DO</span>
           </div>
