7f6dbe4afd8320b8967edc257f8029e465cf8b58
Sujit Sasmal <sujitsasmal40234@gmail.com>
2026-03-29 17:14:26 +0530

    chore: cleanup duplicate import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
-import { Analytics } from "@vercel/analytics/react";
 
 const CharacterModel = lazy(() => import("./components/Character"));
 const MainContainer = lazy(() => import("./components/MainContainer"));
